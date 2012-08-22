570d58169a3080e03edc2f722c45c78e12527933
Marcos Garcia de Lima <marckfree@gmail.com>
2012-08-22 15:24:34 -0300

    Implentando o HTML básico

---
+++ static/index.php
@@ -0,0 +1,194 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Primotech &#8212; Página inicial</title>
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <meta name="author" content="Marcker: marckfree at gmail.com" />
+        <link rel="stylesheet" href="css/reset.css" />
+        <link rel="stylesheet" href="css/text.css" />
+        <link rel="stylesheet" href="css/960.css" />
+        <link rel="stylesheet" href="css/styles.css" />
+        <script src="js/scripts.js"></script> 
+    </head>
+    <body>  
+
+        <div id="container" class="container_12">
+            
+            <div id="header" class="grid_12">  
+                
+                <div class="logotipo">
+
+                </div> <!-- / logotipo -->
+
+                <div class="banner">
+                    <img src="test.jpg" alt="" />
+                </div> <!-- / banner -->
+
+                <div class="menu">
+                    <ul>
+                        <li><a href="#">Primotech 21</a></li>
+                        <li><a href="#">Fabricantes</a></li>
+                        <li><a href="#">Onde comprar</a></li>
+                        <li><a href="#">Aplicações</a></li>
+                        <li><a href="#">Primonews</a></li>
+                        <li><a href="#">Amostras</a></li>
+                        <li><a href="#">Fale conosco</a></li>
+                    </ul>
+                </div> <!-- / menu -->
+
+            </div> <!-- / header -->
+
+            <div id="fabricantes-lateral" class="grid_2">
+                
+                <div class="header-coluna">
+                    <h1>Fabricantes</h1>
+                </div>
+
+                <div class="marcas">
+                    <div class="box-marca">
+                        <img src="test.jpg" alt="" />
+                    </div>
+
+                    <div class="box-marca">
+                        <img src="test.jpg" alt="" />
+                    </div>
+
+                    <div class="box-marca">
+                        <img src="test.jpg" alt="" />
+                    </div>
+
+                    <div class="box-marca">
+                        <img src="test.jpg" alt="" />
+                    </div>
+                </div> <!-- / marcas -->
+
+            </div> <!-- / fabricantes-lateral --> 
+
+            <div id="conteudo-central" class="grid_6">
+
+                <div id="banner-topo-central">
+                    <img src="test.jpg" alt="" />
+                </div> <!-- / banner-topo-central -->
+
+                <div class="destaques">
+
+                    <div class="header-destaque">
+                        <h1>Veja abaixo os produtos que são destaque:</h1>
+                    </div> <!-- / header-destaque -->
+
+                    <div class="boxes-central">
+                        
+                        <div class="box-central">
+
+                            <div class="descricao">
+                                <p>Broadcast &amp; Cable, produção e transmissão HD...</p>
+                            </div>
+                        </div>
+
+                        <div class="box-central">
+
+                            <div class="descricao">
+                                <p>Equipamentos para televisão digital e produção em HD...</p>
+                            </div>
+                        </div>
+
+                        <div class="box-central">
+
+                            <div class="descricao">
+                                <p>Capacitor cerâmico multicamada - série "AMK"...</p>
+                            </div>
+                        </div>
+
+                        <div class="box-central">
+
+                            <div class="descricao">
+                                <p>Equipamentos para televisão digital e produção em HD...</p>
+                            </div>
+                        </div>
+
+                    </div> <!-- / boxes-central -->
+
+                </div> <!-- / destaques -->
+
+                <div id="banner-inferior-central">
+                    <img src="test.jpg" alt="" />
+                </div> <!-- / banner-inferior-central -->
+
+            </div> <!-- / conteudo-central -->
+
+            <div id="comunicacao-lateral" class="grid_4">
+                
+                <div class="ultimas-noticias">
+                    
+                    <div class="header-coluna">
+                        <h1>Últimas notícias</h1>
+                    </div>
+
+                    <div class="box-noticias">
+
+                    </div> <!-- / box-noticias -->
+
+                </div> <!-- / ultimas-noticias -->
+
+                <div class="redes-sociais">
+
+                    <div class="header-coluna">
+                        <h1>Redes sociais</h1>
+                    </div>
+
+                    <div class="lista-icones">
+
+                    </div> <!-- / lista-icones -->
+
+                    <div class="box-facebook">
+
+                    </div> <!-- / box-facebook -->
+
+                </div> <!-- / redes-sociais -->
+                
+                <div class="cotacao">
+                    
+                    <div class="header-coluna">
+                        <h1>Cotação</h1>
+                    </div>
+
+                    <div class="box-cotacao">
+
+                    </div> <!-- / box-cotacao -->
+
+                </div> <!-- / cotacao -->
+
+            </div> <!-- / comunicacao-lateral -->
+
+            <div id="rodape" class="grid_12">
+
+                <div class="menu-rodape">
+                    <ul>
+                        <li><a href="#">Primotech 21</a></li>
+                        <li><a href="#">Fabricantes</a></li>
+                        <li><a href="#">Onde comprar</a></li>
+                        <li><a href="#">Aplicações</a></li>
+                        <li><a href="#">Primonews</a></li>
+                        <li><a href="#">Amostras</a></li>
+                    </ul>             
+                </div> <!-- / menu-rodape -->
+
+                <div class="links-destacados">
+                    <ul>
+                        <li><a href="#">Onde comprar</a></li>
+                        <li><a href="#">Fale conosco</a></li>
+                    </ul>
+                </div> <!-- / links-destacados -->
+
+                <div class="copyright">
+                    <p>Todos os direitos reservados a Primotech</p>
+                </div>
+
+            </div> <!-- / rodape -->
+
+        </div> <!-- / container -->
+        
+    </body>
+</html>
